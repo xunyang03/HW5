a2ac525faecd92f01be9620917822fae6870c3c0
xunyang03 <xunyang03@gmail.com>
2024-12-06 15:02:59 -0500

    attempt VSD method

--- Diffusion/utils/sds_vsd_utils.py
+++ Diffusion/utils/sds_vsd_utils.py
@@ -252,8 +252,8 @@ def sds_vsd_grad_diffuser(unet, noisy_latents, noise, text_embeddings, t, unet_p
 
         # Implement the grad calcualtion based on the Formula (17) provided in the instructions. Also, multiply the result with grad_scale
         # Hint: Understand what noise_pred_phi refers to (No need to dive into predict_noise0_diffuser).
-        grad_ = None
-
+        #grad_ = None
+        grad_ = (noise_pred - noise_pred_phi) * grad_scale
         ################################## Code ends here for VSD ########################################
 
     grad_ = torch.nan_to_num(grad_)
